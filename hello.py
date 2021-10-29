456930216bedefdbca4abe58c3e2ed9d019de99a
mahesh0842 <mahesh767000@gmail.com>
2021-10-29 15:09:57 +0530

    thsi is third commit

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 print("hello")
-a=int(input("enter the value")
-b=int(input("enter the second value"))
+a=int(input("enter the value"))
+b = int (input("enter the second value"))
 c=a+b
 print("the vale is ",c)
+print("welcome")
--- new.py
+++ new.py
@@ -2,3 +2,4 @@ a=int(input("enter"))
 b=int(input("enter"))
 c=a+b
 print(c)
+print("wecome to india")
